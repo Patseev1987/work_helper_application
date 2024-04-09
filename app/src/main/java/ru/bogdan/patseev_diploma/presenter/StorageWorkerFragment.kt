1e2bd74a617aeb52db8f2057e881282f27184792
Patseev1987 <Patseev1987@gmal.com>
2024-04-09 11:07:42 +0300

    add menu navigation in StorageWorkerFragment

--- app/src/main/java/ru/bogdan/patseev_diploma/presenter/StorageWorkerFragment.kt
+++ app/src/main/java/ru/bogdan/patseev_diploma/presenter/StorageWorkerFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import ru.bogdan.patseev_diploma.R
 import ru.bogdan.patseev_diploma.databinding.FragmentStorageWorkerBinding
 
@@ -19,6 +21,14 @@ class StorageWorkerFragment : Fragment() {
     ): View {
         _binding = FragmentStorageWorkerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = view.findNavController()
+
+       binding.navigateView.setupWithNavController(navController)
     }
 
     override fun onDestroy() {
